217e08ff36ea7d62b746f8d60c64de8fd828a070
newtondotcom <asphalt9fr@gmail.com>
2024-06-02 16:02:42 +0200

    action when button pressed

--- components/landing/header.vue
+++ components/landing/header.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-const userLoggedIn = ref(false);
+import processSignIn from '@/lib/signin'
+
+async function process(){
+    await processSignIn()
+}
 </script>
 
 <template>
@@ -51,14 +55,9 @@ const userLoggedIn = ref(false);
           >
           <svg class="w-6 h-6" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>ic_fluent_dark_theme_24_regular</title> <desc>Created with Sketch.</desc> <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="ic_fluent_dark_theme_24_regular" fill="#212121" fill-rule="nonzero"> <path d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z" id="🎨-Color"> </path> </g> </g> </g></svg>
           </button>-->
-            <NuxtLink
-              v-if = "!userLoggedIn"
-              to="/auth/login"
-            >
-            <Button class="px-5 py-2.5">
+            <Button @click="process" class="px-5 py-2.5">
               Login or Register
             </Button>
-            </NuxtLink>
   
             <!--
             <NuxtLink
@@ -69,7 +68,7 @@ const userLoggedIn = ref(false);
               Register
             </NuxtLink>
             -->
-
+<!--
             <NuxtLink
               v-if = "userLoggedIn"
               to="/dashboard"
@@ -78,6 +77,7 @@ const userLoggedIn = ref(false);
               Dashboard
             </Button>
             </NuxtLink>
+                  -->
  
           </div>
   
