08c6134619716625257bc3a37b23237c1f552ee3
newtondotcom <asphalt9fr@gmail.com>
2024-06-06 21:17:31 +0200

    terms of services

---
+++ pages/tos.vue
@@ -0,0 +1,68 @@
+<template>
+<h1 class="text-4xl m-4 font-bold">Terms and Conditions</h1>
+
+<p class="text-xl mx-2">These terms and conditions ("Terms", "Agreement") are an agreement between Software Developer ("Mobile Application Developer", "us", "we", "our" or "Software") and you ("User", "you" or "your").</p>
+
+<p class="text-xl mx-2">This Agreement sets forth the general terms and conditions of your use of Yogocap, an AI-powered service that automatically subtitles video, and any of its products or services (collectively, "Software", "Mobile Application" or "Services").</p>
+
+<h2 class="text-3xl m-2 font-bold">Accounts and Membership</h2>
+
+<h3 class="text-2xl mx-2 font-semibold">Account Creation</h3>
+<p class="text-xl mx-2">When you create an account in Yogocap, you are responsible for maintaining the security of your account and for all activities that occur under the account. Any false contact information may result in the termination of your account.</p>
+
+<h3 class="text-2xl mx-2 font-semibold">Unauthorized Use</h3>
+<p class="text-xl mx-2">You must immediately notify us of any unauthorized uses of your account or any other breaches of security. We are not liable for any acts or omissions by you, including any damages incurred as a result of such acts or omissions.</p>
+
+<h3 class="text-2xl mx-2 font-semibold">Account Suspension or Termination</h3>
+<p class="text-xl mx-2">We may suspend, disable, or delete your account (or any part thereof) if we determine that you have violated any provision of this Agreement or that your conduct or content would tend to damage our reputation and goodwill. If we delete your account for the foregoing reasons, you may not re-register for our Services. We may block your email address and Internet protocol address to prevent further registration.</p>
+
+<h2 class="text-3xl m-2 font-bold">User Content</h2>
+
+<p class="text-xl mx-2">We do not own any data, information, or material ("Content") that you submit in the Software in the course of using the Service. You shall have sole responsibility for the accuracy, quality, integrity, legality, reliability, appropriateness, and intellectual property ownership or right to use all submitted Content.</p>
+
+<p class="text-xl mx-2">We may monitor Content in the Software submitted or created using our Services by you. Unless specifically permitted by you, your use of the Software does not grant us the license to use, reproduce, adapt, modify, publish, or distribute the Content created by you or stored in your user account for commercial, marketing, or any similar purpose. However, you grant us permission to access, copy, distribute, store, transmit, reformat, display, and perform the Content of your user account solely as required for the purpose of providing the Services to you.</p>
+
+<p class="text-xl mx-2">Without limiting any of those representations or warranties, we have the right, though not the obligation, to, in our own sole discretion, refuse or remove any Content that, in our reasonable opinion, violates any of our policies or is in any way harmful or objectionable.</p>
+
+<h2 class="text-3xl m-2 font-bold">Backups</h2>
+
+<p class="text-xl mx-2">We perform regular backups of the Content and will do our best to ensure completeness and accuracy of these backups. In the event of hardware failure or data loss, we will restore backups automatically to minimize the impact and downtime.</p>
+
+<h2 class="text-3xl m-2 font-bold">Intellectual Property Rights</h2>
+
+<p class="text-xl mx-2">This Agreement does not transfer to you any intellectual property owned by Software Developer or third parties, and all rights, titles, and interests in and to such property will remain (as between the parties) solely with Software Developer.</p>
+
+<p class="text-xl mx-2">All trademarks, service marks, graphics, and logos used in connection with our Software or Services are trademarks or registered trademarks of Software Developer or Software Developer licensors. Other trademarks, service marks, graphics, and logos used in connection with our Software or Services may be the trademarks of other third parties.</p>
+
+<p class="text-xl mx-2">Your use of our Software and Services grants you no right or license to reproduce or otherwise use any Software Developer or third-party trademarks.</p>
+
+<h2 class="text-3xl m-2 font-bold">Dispute Resolution</h2>
+
+<p class="text-xl mx-2">The formation, interpretation, and performance of this Agreement and any disputes arising out of it shall be governed by the substantive and procedural laws of India without regard to its rules on conflicts or choice of law and, to the extent applicable, the laws of India. The exclusive jurisdiction and venue for actions related to the subject matter hereof shall be the state and federal courts located in India, and you hereby submit to the personal jurisdiction of such courts. You hereby waive any right to a jury trial in any proceeding arising out of or related to this Agreement.</p>
+
+<p class="text-xl mx-2">The United Nations Convention on Contracts for the International Sale of Goods does not apply to this Agreement.</p>
+
+<h2 class="text-3xl m-2 font-bold">Changes and Amendments</h2>
+
+<p class="text-xl mx-2">We reserve the right to modify this Agreement or its policies relating to the Software or Services at any time, effective upon posting of an updated version of this Agreement in the Software. When we do, we will post a notification in our Software. Continued use of the Software after any such changes shall constitute your consent to such changes.</p>
+
+<h2 class="text-3xl m-2 font-bold">Acceptance of These Terms</h2>
+
+<p class="text-xl mx-2">You acknowledge that you have read this Agreement and agree to all its terms and conditions. By using the Software or its Services you agree to be bound by this Agreement. If you do not agree to abide by the terms of this Agreement, you are not authorized to use or access the Software and its Services.</p>
+
+<h2 class="text-3xl m-2 font-bold">Contacting Us</h2>
+
+<p class="text-xl mx-2">If you have any questions about this Agreement, please contact us from the contact us page.</p>
+
+
+<p class="text-xl mx-2">This document was last updated on June 06, 2024</p>
+
+<div class="h-20" />
+
+</template>
+
+<style>
+        h1, h2 {
+            color: #333;
+        }
+</style>
