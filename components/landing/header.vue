aa01b289362c0a306363eaa0179550d1f357f400
newtondotcom <asphalt9fr@gmail.com>
2024-06-11 21:54:17 +0200

    badge free trial on register button

--- components/landing/header.vue
+++ components/landing/header.vue
@@ -57,9 +57,14 @@ async function process(){
           >
           <svg class="w-6 h-6" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>ic_fluent_dark_theme_24_regular</title> <desc>Created with Sketch.</desc> <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="ic_fluent_dark_theme_24_regular" fill="#212121" fill-rule="nonzero"> <path d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z" id="🎨-Color"> </path> </g> </g> </g></svg>
           </button>-->
-            <Button @click="process" class="px-5 py-2.5">
-              Login or Register
-            </Button>
+  <div class="relative inline-block">
+    <div class="absolute top-0 left-0 rounded-full -translate-y-1/3 -translate-x-1/2 border-2 border-primary bg-white text-primary px-2 py-[2px] text-xs z-10">
+      Free trial
+    </div>
+    <Button @click="process" class="px-5 py-2.5">
+      Login or Register
+    </Button>
+  </div>
   
             <!--
             <NuxtLink
